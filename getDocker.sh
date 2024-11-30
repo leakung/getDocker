87f4313f297d669d31def227abbeea3683cefe57
Atsadawat N. <39398225+leakung@users.noreply.github.com>
2024-11-30 17:39:00 +0700

    Update getDocker.sh

--- getDocker.sh
+++ getDocker.sh
@@ -15,4 +15,4 @@ echo \
 sudo apt update
 
 # Install the Docker packages:
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
